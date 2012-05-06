2faf38c16c964f2353892af4c152b7829699b32f
Juan Lupion <juan.lupion@the-cocktail.com>
2012-05-06 19:29:45 +0200

    Credenciales de Heroku en config de entorno, no en initializer

--- config/initializers/twitter_tokens.rb
+++ config/initializers/twitter_tokens.rb
@@ -1,16 +1,8 @@
-# Produccion
-# OAUTH_ACCESS_TOKEN = "134175860-0Wzs51rpR88WdsbOIY5pQGL1CGAMw3ULtpEX6YSE"
-# OAUTH_ACCESS_SECRET_TOKEN = "AukFvpY5E9a6qmzu9F3wEQls6clpt4zSZlvaSBo"
-# OAUTH_CONSUMER_TOKEN = "jFnahsW0MyWGhvkwbgA"
-# OAUTH_CONSUMER_SECRET_TOKEN = "YeNS4OoNOOMkOAyRuBWhMsYHHN7JyoaPwWYdDRe8"
-
-# Beta (metroroto_stag)
-
 Twitter.configure do  |config|
-  config.consumer_key = "OG7MVMggXQu0K2wUoxQeg"
-  config.consumer_secret  =  "QDj1XjD5iWXco0xFjThTn5LwZV0UVosWUPadLrcQac"
-  config.oauth_token =  "146799396-GV1DfgZRQDak43ZvjnTHtOt7hgHwJXZB5CNTreeY"
-  config.oauth_token_secret =  "eCTp0NeOhIHNhjTGk9A9kw3ZrfFPTcfiqFKKTU13L3g"
+  config.consumer_key = ENV['TWITTER_CONSUMER_KEY'] 
+  config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+  config.oauth_token = ENV['TWITTER_OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['TWITTER_OAUTH_TOKEN_SECRET']
 end
 
 
